b591eb398f24c131705a367d6190ac3bf1c9f266
Pedro Castilho <cstl.px@gmail.com>
2016-06-09 21:31:24 -0300

    Production config from Heroku env vars

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,8 @@ defmodule CoreoServer.UserSocket do
   channel "updates:*", CoreoServer.UpdateChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
